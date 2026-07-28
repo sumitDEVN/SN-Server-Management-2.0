9747477525c90bc67b0361d31eda5214302578ac
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 23:05:27 +0600

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# ====================================
-# SN SERVER MANAGEMENT 2.0
-# Configuration
-# ====================================
-
 PANEL_NAME="SN Server Management"
 
 VERSION="2.0"
@@ -14,5 +9,7 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DB_DIR="$BASE_DIR/database"
 
 USER_DB="$DB_DIR/users.db"
+
+LOG_DIR="$BASE_DIR/logs"
 
 SSH_PORT=22
