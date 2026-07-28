781a292b5851c804f127e93d518144d640c0e1c1
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 23:06:47 +0600

    Update database.sh

--- lib/database.s
+++ lib/database.s
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # ====================================
-# SN SERVER MANAGEMENT 2.0
+# SN SERVER MANAGEMENT 2.1
 # Database System
 # ====================================
 
 source config.sh
+source lib/logger.sh
 
 
 init_database(){
@@ -25,28 +26,94 @@ init_database(){
 
 
 
-add_record(){
+add_user_record(){
 
-    username=$1
-    expiry=$2
-
-    echo "$username|$expiry" >> "$USER_DB"
-
-}
+    username="$1"
+    expiry="$2"
+    status="$3"
 
 
+    created=$(date "+%Y-%m-%d")
 
-remove_record(){
 
-    username=$1
+    echo "$username|$expiry|$status|$created" >> "$USER_DB"
 
-    sed -i "/^$username|/d" "$USER_DB"
+
+    log_action "ADD_USER_DB" "$username" "SUCCESS"
 
 }
 
 
 
-list_records(){
+remove_user_record(){
+
+    username="$1"
+
+
+    if grep -q "^$username|" "$USER_DB"
+    then
+
+        sed -i "/^$username|/d" "$USER_DB"
+
+        log_action "REMOVE_USER_DB" "$username" "SUCCESS"
+
+        return 0
+
+    else
+
+        log_action "REMOVE_USER_DB" "$username" "FAILED"
+
+        return 1
+
+    fi
+
+}
+
+
+
+user_exists(){
+
+    username="$1"
+
+
+    if grep -q "^$username|" "$USER_DB"
+    then
+        return 0
+    else
+        return 1
+    fi
+
+}
+
+
+
+get_user_data(){
+
+    username="$1"
+
+
+    grep "^$username|" "$USER_DB"
+
+}
+
+
+
+update_user_record(){
+
+    username="$1"
+    new_data="$2"
+
+
+    sed -i "s/^$username|.*/$new_data/" "$USER_DB"
+
+
+    log_action "UPDATE_USER_DB" "$username" "SUCCESS"
+
+}
+
+
+
+list_database(){
 
     cat "$USER_DB"
 
